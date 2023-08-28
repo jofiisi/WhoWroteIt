73375fe1535d5cf4a90b0267942d07f8db0b4915
jofiisi <jofiisi.dkt@gmail.com>
2023-08-28 21:32:23 +0200

    truly add it

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,14 @@
 import { View, Text} from "react-native";
+import udpSocket from "react-native-udp";
 
 const Search = ({ navigation }) => {
+    let PORT = 6024;
+
+    const client = udpSocket.createSocket('udp4');
+    client.bind(PORT);
+    client.on('message', function(msg, rinfo) {
+        console.log('Message received', msg)
+      })
     return (
         <View>
             <Text>

40a560971ce51a2f73f0ce76040fe947d5b7bad7
jofiisi <jofiisi.dkt@gmail.com>
2023-09-04 17:27:39 +0200

    lobby managment basics, still working

--- src/Host.js
+++ src/Host.js
@@ -5,27 +5,38 @@ import udpSocket from "react-native-udp";
 const Host = ({ navigation }) => {
     let PORT = 6024;
     const [lobbyName, setLobbyname] = useState('');
+    const [server, setServer] = useState(null);
 
     useEffect(() => {
         const server = udpSocket.createSocket('udp4');
 
-        server.bind(PORT, () => {
-            server.setBroadcast(true)
+        server.bind(PORT);
+
+        server.once('listening', function () {
+            server.setBroadcast(true);
+            server.send('Hosting', 0, 7, 6024, '255.255.255.255', function (err) {
+                if (err) throw err
+
+                console.log('Message sent!')
+            })
         })
 
-        server.once('listening', function() {
-            
-            server.send('Hosting', 0, 7, 6024, '255.255.255.255', function(err) {
-              if (err) throw err
-          
-              console.log('Message sent!')
-            })
-          })
+        setServer(server);
 
         return () => {
             server.close();
         }
     }, []);
+
+    const startLobby = () => {
+        server.send('lb' + lobbyName, 0, lobbyName.length + 2, PORT, '255.255.255.255', function (err) {
+            if (err) {
+                console.error("Error sending message:", err);
+            } else {
+                console.log('Message sent!')
+            }
+        });
+    };
 
     return (
         <View>
@@ -38,6 +49,7 @@ const Host = ({ navigation }) => {
             />
             <Button
                 title="start Lobby"
+                onPress={startLobby}
             />
         </View>
     );
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import udpSocket from "react-native-udp";
 
 const Search = ({ navigation }) => {
     let PORT = 6024;
+
+    const [lobbys, setLobbys] = useState([]);
 
     useEffect(() => {
         const client = udpSocket.createSocket('udp4');
@@ -12,6 +14,10 @@ const Search = ({ navigation }) => {
 
         client.on('message', (msg, rinfo) => {
             console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
+            if(msg.slice(0, 2) == 'lb')
+            {
+                console.log("found lobby");
+            }
         });
 
         client.on('error', (err) => {

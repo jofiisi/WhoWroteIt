66b4d0edc3d85dedaee3b89185fdabdc22381a61
jofiisi <jofiisi.dkt@gmail.com>
2023-09-26 22:02:25 +0200

    fix lobbys, useState was problem

--- src/Search.js
+++ src/Search.js
@@ -14,47 +14,38 @@ const Search = ({ navigation }) => {
 
         client.on('message', (msg, rinfo) => {
             console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
-            let msgLobby = msg.slice(2);
+            let msgLobby = msg.slice(2).toString();
             if (msg.slice(0, 2) == 'lb') {
                 console.log("found lobby");
-                if (lobbys.every(element => element.message != msgLobby))
-                {
-                    console.log('Checked message:', msgLobby);
-                    
-                    setLobbys(prevLobbys => [...prevLobbys, { message: msgLobby, address: rinfo.address }]);
-                }else {
-                    console.log('Lobby already listed');
-                }
+
+                // Use a callback function to ensure the correct value is used
+                setLobbys(prevLobbys => {
+                    if (!prevLobbys.some(element => element.message === msgLobby)) {
+                        console.log('Checked message:', msgLobby);
+                        return [...prevLobbys, { message: msgLobby, address: rinfo.address }];
+                    } else {
+                        console.log('Lobby already listed');
+                        return prevLobbys;
+                    }
+                });
             }
         });
-
-        client.on('error', (err) => {
-            console.log('Socket error:', err);
-        });
-
-        return () => {
-            client.close();
-        };
-    }, []);
-    
-
+    }, []); // Empty dependency array, so this effect only runs once
+//comments by chatgpt, asychronus state cause a headache
     return (
         <View>
             <Text>
                 Search
             </Text>
-            {console.log(lobbys)}
             {
                 lobbys.length > 0 ?
                     (
                         lobbys.map((lobby, index) => (
-                            
-                            <View key={index}>  //every element gets a unique key so its easier to render
+                            <View key={index}>
                                 <Text>{String(lobby.message)}</Text>
                             </View>
                         ))
                     ) : (<Text>No Lobbys found yet</Text>)
-
             }
         </View>
     );

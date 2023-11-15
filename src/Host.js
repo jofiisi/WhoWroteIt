ec94f511bf334351fcb0e9ebde0a657e565d7441
jofiisi <jofiisi.dkt@gmail.com>
2023-11-15 21:16:57 +0100

    setBroadcast generates error

--- src/Host.js
+++ src/Host.js
@@ -3,13 +3,13 @@ import { View, Text, TextInput, Button } from "react-native";
 import udpSocket from "react-native-udp";
 
 //remove when not using an Androidstudioemulator
-const bcIP = '10.0.2.2'
+const bcIP = '255.255.255.255'
 
 const Host = ({ navigation }) => {
     let PORT = 6024;
     const [lobbyName, setLobbyname] = useState('');
     const [server, setServer] = useState(null);
-    const [indexListener, setIndexListener] = useState(null);
+    const [indexListener, setIndexListener] = useState(0);
     const repeatRef = useRef(null);
     const lobbyNameRef = useRef('');
     const [startedLobby, setStartedLobby] = useState(false);
@@ -22,7 +22,7 @@ const Host = ({ navigation }) => {
         server.bind(PORT);
 
         server.once('listening', function () {
-            server.setBroadcast(true);
+            //server.setBroadcast(true);
             server.send('Hosting', 0, 7, 6024, bcIP, function (err) {
                 if (err) throw err;
                 console.log('Message sent!');
@@ -32,23 +32,22 @@ const Host = ({ navigation }) => {
         server.on('message', (msg, rinfo) => {
             console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
             let msgText = msg.slice(2).toString();
-
             if (msg.slice(0, 2) == 'MG') {
                 console.log("found Text");
                 // Use a callback function to ensure the correct value is used
-                const updatedUsers = users.map((user) => {
-                    if((user.address == rinfo.address) && (users[indexListener].address != rinfo.address))
-                    {
-                        if(user.text != null)
-                        {
-                            console.log(users);
-                            return {...user, text: msgText};
+                setUsers(prevUsers => {
+                    console.log(indexListener)
+                    const updatedUsers = prevUsers.map(user => {
+                        console.log(indexListener)
+                        if ((user.address === rinfo.address)/* && (prevUsers[indexListener].address !== rinfo.address)*/) {
+                            if (user.text == null) {
+                                return { ...user, text: msgText };
+                            }
                         }
-                    }
-                    return user;
+                        return user;
+                    });
+                    return updatedUsers;
                 });
-
-                setUsers(updatedUsers);
             }
 
             if (msg.slice(0, 2) == 'UN') {
@@ -70,6 +69,7 @@ const Host = ({ navigation }) => {
         setServer(server);
 
         return () => {
+            console.log("return to homescreen")
             server.close();
             clearInterval(repeatRef.current);
         };
